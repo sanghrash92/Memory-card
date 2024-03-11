8237a68c3f7127ac3f9edfb8405c9bb39ff777e8
Sanji <sanghrash92@gmail.com>
2024-03-11 21:37:19 +0000

    Final project

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/App.css'
+import './styles/welcome.css'
+import { Welcome } from "./components/Welcome"
+import { Images } from "./components/images"
+import { Loading } from "./components/loading"
+import { 
+  useState,
+  useEffect } from 'react'
+
+  const Header = () => {
+    return (
+      <header>
+        <h1>Memory game</h1>
+        <h2>Pick all characters without repeating.</h2>
+        <br />
+      </header>
+    )
+  }
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [imageList, setImageList] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchRequest = async (pageNum) => {
+    const data = await fetch(
+      `https://api.unsplash.com/search/photos?page=1&per_page=${pageNum}&query=Golf&client_id=-RGBeeYDvRYQY2cWmfktUdb4TJigFFWyfZ2NGF-1CWQ`
+    );
+    const dataJ = await data.json();
+    const result =  await dataJ.results;
+    setImageList(result);
+  };
+
+  useEffect(() => {
+    fetchRequest()
+  }, [])
+
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 4000)
+  }, [])
+  if(loading) {
+    return (
+      <div>
+        <Loading />
+      </div>
+    )
+  }
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Header />
+      <div className="difficulty-level">
+        <Welcome fetchReq={fetchRequest} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <br />
+      <main>
+        <Images list={imageList}/>
+      </main>
     </>
   )
 }
